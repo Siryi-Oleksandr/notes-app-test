f348dbe090f928c8723edd744bf4f533d5b82450
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-05-07 17:57:42 +0300

    add favicon and create base components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from '../../GlobalStyle';
+import { Container } from './App.styled';
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello bro</h1>
+
+      <GlobalStyle />
+    </Container>
+  );
+}
+
+export default App;
